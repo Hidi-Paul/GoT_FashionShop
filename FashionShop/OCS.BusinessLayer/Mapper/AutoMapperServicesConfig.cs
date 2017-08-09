92e8543df7262c9df99a27fc4a17be52a99f4480
Hidi Paul <paul.hidi@endava.com>
2017-08-09 14:31:09 +0300

    Added Auto-mapper

--- FashionShop/OCS.BusinessLayer/Mapper/AutoMapperServicesConfig.cs
+++ FashionShop/OCS.BusinessLayer/Mapper/AutoMapperServicesConfig.cs
@@ -15,7 +15,7 @@ namespace OCS.BusinessLayer.Mapping
             {
                 //cfg.CreateMap<Product,ProductModel>(); 
                 //cfg.CreateMap<ProductModel,Product>();
-                
+
 
             });
         }
