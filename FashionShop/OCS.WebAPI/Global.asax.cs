bb0cd0523b6039779d413b97d8f178231067045d
Hidi Paul <paul.hidi@endava.com>
2017-08-09 11:20:36 +0300

    Added Unity Injector to the project

--- FashionShop/OCS.WebAPI/Global.asax.cs
+++ FashionShop/OCS.WebAPI/Global.asax.cs
@@ -11,6 +11,7 @@ namespace OCS.WebAPI
     {
         protected void Application_Start()
         {
+            UnityConfig.RegisterComponents();
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
