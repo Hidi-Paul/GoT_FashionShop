d3551902e44b87f4a31b5e45841e1836b476b5b7
Hidi Paul <paul.hidi@endava.com>
2017-08-10 13:40:53 +0300

    Registered IBrandRepository and ICategoryRepository to Unity Injector

--- FashionShop/OCS.WebAPI/App_Start/UnityConfig.cs
+++ FashionShop/OCS.WebAPI/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using Microsoft.Practices.Unity;
 using System.Web.Http;
 using Unity.WebApi;
+using OCS.DataAccess.Repositories;
 
 namespace OCS.WebAPI
 {
@@ -9,13 +10,16 @@ namespace OCS.WebAPI
         public static void RegisterComponents()
         {
 			var container = new UnityContainer();
-            
+
             // register all your components with the container here
             // it is NOT necessary to register your controllers
-            
-            // e.g. container.RegisterType<ITestService, TestService>();
-            
 
+            // e.g. container.RegisterType<ITestService, TestService>();
+
+
+
+            container.RegisterType<IBrandRepository, BrandRepository>();
+            container.RegisterType<ICategoryRepository, CategoryRepository>();
 
             GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
         }
