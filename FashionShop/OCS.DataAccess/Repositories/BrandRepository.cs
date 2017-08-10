94937b81f2ce1ae00063a33aa272b38512ee7223
Hidi Paul <paul.hidi@endava.com>
2017-08-10 13:29:15 +0300

    Added Repository for Brands

---
+++ FashionShop/OCS.DataAccess/Repositories/BrandRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCS.DataAccess.Repositories
+{
+    public class BrandRepository : IBrandRepository
+    {
+        public Brand GetBrandById(Guid id)
+        {
+            using(DataModel db=new DataModel())
+            {
+                var brand = db.Brands.Where(x => x.BrandID == id).First();
+                return brand;
+            }
+        }
+        public Brand GetBrandByName(string name)
+        {
+            using(DataModel db=new DataModel())
+            {
+                var brand = db.Brands.Where(x => x.BrandName == name).First();
+                return brand;
+            }
+        }
+        public IEnumerable<Brand> GetAllBrands()
+        {
+            using(DataModel db=new DataModel())
+            {
+                return db.Brands;
+            }
+        }
+        public void SaveBrand(Brand brand)
+        {
+            using(DataModel db=new DataModel())
+            {
+                db.Brands.Add(brand);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ FashionShop/OCS.DataAccess/Repositories/IBrandRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCS.DataAccess.Repositories
+{
+    public interface IBrandRepository
+    {
+        Brand GetBrandById(Guid id);
+        Brand GetBrandByName(string name);
+        IEnumerable<Brand> GetAllBrands();
+
+        void SaveBrand(Brand brand);
+    }
+}
