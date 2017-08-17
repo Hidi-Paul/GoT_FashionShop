63d71726c41118c6d6f76f2dc09917d64d040087
Hidi Paul <paul.hidi@endava.com>
2017-08-17 15:26:19 +0300

    Added a couple of useful AJAX requests

--- FashionShop/OCS.MVC/Scripts/Store.js
+++ FashionShop/OCS.MVC/Scripts/Store.js
@@ -1,6 +1,8 @@
 ﻿var button = document.getElementById("ASD")
 button.addEventListener("click", myFunc);
 
+var serverAddr ="https://localhost:44300/"
+
 function myFunc() {
 
     var xhr = new XMLHttpRequest();
@@ -20,20 +22,51 @@ function myFunc() {
 }
 
 window.onload = function () {
-    loadCategories();
+    getBrands();
+    getCategs();
 };
 
-function loadCategories() {
+function getBrands() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', serverAddr+'GetAllBrands');
+    xhr.setRequestHeader('Access-Control-Allow-Headers', '*');
+    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            console.log("brands:", xhr.response);
+            initBrands(xhr.response);
+        }
+        else {
+            alert('Brand Request failed.  Returned status of ' + xhr.status);
+        }
+    };
+    xhr.send();
+}
+function getCategs() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', serverAddr + 'GetAllCategories');
+    xhr.setRequestHeader('Access-Control-Allow-Headers', '*');
+    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            console.log("categs:", xhr.response);
+            initCategs(xhr.response);
+        }
+        else {
+            alert('Categories Request failed.  Returned status of ' + xhr.status);
+        }
+    };
+    xhr.send();
+}
+function initBrands(brands) {
     var colBrand = document.getElementById("collapsibleBrands");
-    var colCateg = document.getElementById("collapsibleCategs");
-
     var Brandies = colBrand.getElementsByClassName("smallThingies");
+
+    console.log("Brandies", Brandies);
+}
+function initCategs(categs) {
+    var colCateg = document.getElementById("collapsibleCategs");
     var Categies = colCateg.getElementsByClassName("smallThingies");
 
-    console.log("Brandies", Brandies)
-    console.log("Categies", Categies)
-
-
-
-    console.log("Left Bar initialized");
-};
+    console.log("Categies", Categies);
+}
