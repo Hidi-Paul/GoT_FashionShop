37ad6d3c3eaca07a97d3ea333e3d9322eb6361fb
Hidi Paul <paul.hidi@endava.com>
2017-08-22 12:49:19 +0300

    Added model validations on Brands, Categories and Products

--- FashionShop/OCS.BusinessLayer/Models/ProductModel.cs
+++ FashionShop/OCS.BusinessLayer/Models/ProductModel.cs
@@ -1,22 +1,33 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace OCS.BusinessLayer.Models
 {
     public class ProductModel
     {
         public Guid ProductID { get; set; }
 
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Product Name required")]
+        [StringLength(50,ErrorMessage ="Product Name is too long!")]
         public string ProductName { get; set; }
 
+        [Required(AllowEmptyStrings = false, ErrorMessage ="Price is mandatory")]
+        [Range(0,9999,ErrorMessage ="Invalid Price Range!")]
         public double ProductPrice { get; set; }
 
+        [StringLength(10)]
         public string Gender { get; set; }
-
-        public string Color { get; set; }
-
+        
+        [Required(AllowEmptyStrings = false, ErrorMessage ="Brand is mandatory")]
+        [StringLength(50)]
         public string Brand { get; set; }
 
+        [Required(AllowEmptyStrings = false, ErrorMessage ="Category is mandatory")]
+        [StringLength(50)]
         public string Category { get; set; }
 
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please add an image.")]
+        [StringLength(200)]
         public string Image { get; set; }
         
     }
